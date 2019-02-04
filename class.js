487369b4f94643f75f928798bde0ca6af0f67246
najiba1221 <46886664+najiba1221@users.noreply.github.com>
2019-02-04 23:40:35 +0300

    Add files via upload

---
+++ class.js
@@ -0,0 +1,20 @@
+class Author{
+
+    constructor(firstName , lastName){
+    this.firstName=firstName;
+    this.lastName-lastName;
+    }
+    
+    }
+
+    class Book{
+    
+    constructor(isbn, title, numberOfPages){
+    this.isbn=isbn;
+    this.title=title;
+    this.numberOfPages=numberOfPages;
+    } 
+}
+discribe(){
+    return
+}
