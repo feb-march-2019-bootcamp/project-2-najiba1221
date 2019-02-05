b1ea8c6c60c7f46b5bcb5d0668f2ef27e7926cd8
najiba1221 <46886664+najiba1221@users.noreply.github.com>
2019-02-05 23:37:44 +0300

    Add files via upload

---
+++ class school.js
@@ -0,0 +1,30 @@
+class school{
+    constructor(){
+        this.student=[];
+        this.teacher=[];
+    }
+    addteacher(){
+         addteacher.push(teacher);
+        if(!(teacher instance of teacher))
+        throw error("teacher should be teacher")
+
+    }
+    fireteacher(){
+        this teacher.Splice(index)
+    }
+
+
+    rigisterstudent()
+} rigisterstudent.push(student){
+
+    if(!( student instance of student))
+    throw error("student should be student");
+}
+firestudent(){
+    this student.splice(index);
+}
+console.log(addteacher,fireteacher);
+console.log(rigisterstudent,firestudent)
+
+
+}
